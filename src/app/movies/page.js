eb8a8173343866304fa2c31b0ccbcb1b12ac9fce
details.wes <details.wes@gmail.com>
2023-12-28 09:56:23 +0530

    edit button added in product listing

--- src/app/movies/page.js
+++ src/app/movies/page.js
@@ -15,53 +15,95 @@ const MyMovies = () => {
         </div>
 
         <div className="mx-auto grid grid-cols-2 lg:grid-cols-4 gap-5 lg:gap-6">
-            <div className="bg-card p-2 rounded-xl cursor-pointer hover:bg-[#1e414e] transition-all">
+            <div className="group bg-card p-2 rounded-xl cursor-pointer hover:bg-[#1e414e] transition-all">
               <Image className='w-full h-auto rounded-xl' src={ProductImage} alt='product-image' />
-              <h6 className='mt-4 mb-2 mx-2'>Movie 1</h6>
-              <p className='mb-2 mx-2'>2021</p>
+              <div className='flex justify-between items-start'>
+              <div className='mt-4 mb-2'>
+                <h6 className='mb-2 mx-2'>Movie 1</h6>
+                <p className='mx-2'>2021</p>
+              </div>
+              <button type="button" class="invisible group-hover:visible mt-4 text-white bg-primary hover:bg-primary-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Edit</button>
+              </div>              
             </div>
 
-            <div className="bg-card p-2 rounded-xl cursor-pointer hover:bg-[#1e414e] transition-all">
+            <div className="group bg-card p-2 rounded-xl cursor-pointer hover:bg-[#1e414e] transition-all">
               <Image className='w-full h-auto rounded-xl' src={ProductImage} alt='product-image' />
-              <h6 className='mt-4 mb-2 mx-2'>Movie 1</h6>
-              <p className='mb-2 mx-2'>2021</p>
+              <div className='flex justify-between items-start'>
+              <div className='mt-4 mb-2'>
+                <h6 className='mb-2 mx-2'>Movie 1</h6>
+                <p className='mx-2'>2021</p>
+              </div>
+              <button type="button" class="invisible group-hover:visible mt-4 text-white bg-primary hover:bg-primary-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Edit</button>
+              </div>              
             </div>
 
-            <div className="bg-card p-2 rounded-xl cursor-pointer hover:bg-[#1e414e] transition-all">
+            <div className="group bg-card p-2 rounded-xl cursor-pointer hover:bg-[#1e414e] transition-all">
               <Image className='w-full h-auto rounded-xl' src={ProductImage} alt='product-image' />
-              <h6 className='mt-4 mb-2 mx-2'>Movie 1</h6>
-              <p className='mb-2 mx-2'>2021</p>
+              <div className='flex justify-between items-start'>
+              <div className='mt-4 mb-2'>
+                <h6 className='mb-2 mx-2'>Movie 1</h6>
+                <p className='mx-2'>2021</p>
+              </div>
+              <button type="button" class="invisible group-hover:visible mt-4 text-white bg-primary hover:bg-primary-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Edit</button>
+              </div>              
             </div>
 
-            <div className="bg-card p-2 rounded-xl cursor-pointer hover:bg-[#1e414e] transition-all">
+            <div className="group bg-card p-2 rounded-xl cursor-pointer hover:bg-[#1e414e] transition-all">
               <Image className='w-full h-auto rounded-xl' src={ProductImage} alt='product-image' />
-              <h6 className='mt-4 mb-2 mx-2'>Movie 1</h6>
-              <p className='mb-2 mx-2'>2021</p>
+              <div className='flex justify-between items-start'>
+              <div className='mt-4 mb-2'>
+                <h6 className='mb-2 mx-2'>Movie 1</h6>
+                <p className='mx-2'>2021</p>
+              </div>
+              <button type="button" class="invisible group-hover:visible mt-4 text-white bg-primary hover:bg-primary-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Edit</button>
+              </div>              
             </div>
 
-            <div className="bg-card p-2 rounded-xl cursor-pointer hover:bg-[#1e414e] transition-all">
+            <div className="group bg-card p-2 rounded-xl cursor-pointer hover:bg-[#1e414e] transition-all">
               <Image className='w-full h-auto rounded-xl' src={ProductImage} alt='product-image' />
-              <h6 className='mt-4 mb-2 mx-2'>Movie 1</h6>
-              <p className='mb-2 mx-2'>2021</p>
+              <div className='flex justify-between items-start'>
+              <div className='mt-4 mb-2'>
+                <h6 className='mb-2 mx-2'>Movie 1</h6>
+                <p className='mx-2'>2021</p>
+              </div>
+              <button type="button" class="invisible group-hover:visible mt-4 text-white bg-primary hover:bg-primary-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Edit</button>
+              </div>              
             </div>
 
-            <div className="bg-card p-2 rounded-xl cursor-pointer hover:bg-[#1e414e] transition-all">
+            <div className="group bg-card p-2 rounded-xl cursor-pointer hover:bg-[#1e414e] transition-all">
               <Image className='w-full h-auto rounded-xl' src={ProductImage} alt='product-image' />
-              <h6 className='mt-4 mb-2 mx-2'>Movie 1</h6>
-              <p className='mb-2 mx-2'>2021</p>
+              <div className='flex justify-between items-start'>
+              <div className='mt-4 mb-2'>
+                <h6 className='mb-2 mx-2'>Movie 1</h6>
+                <p className='mx-2'>2021</p>
+              </div>
+              <button type="button" class="invisible group-hover:visible mt-4 text-white bg-primary hover:bg-primary-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Edit</button>
+              </div>              
             </div>
 
-            <div className="bg-card p-2 rounded-xl cursor-pointer hover:bg-[#1e414e] transition-all">
+            <div className="group bg-card p-2 rounded-xl cursor-pointer hover:bg-[#1e414e] transition-all">
               <Image className='w-full h-auto rounded-xl' src={ProductImage} alt='product-image' />
-              <h6 className='mt-4 mb-2 mx-2'>Movie 1</h6>
-              <p className='mb-2 mx-2'>2021</p>
+              <div className='flex justify-between items-start'>
+              <div className='mt-4 mb-2'>
+                <h6 className='mb-2 mx-2'>Movie 1</h6>
+                <p className='mx-2'>2021</p>
+              </div>
+              <button type="button" class="invisible group-hover:visible mt-4 text-white bg-primary hover:bg-primary-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Edit</button>
+              </div>              
             </div>
 
-            <div className="bg-card p-2 rounded-xl cursor-pointer hover:bg-[#1e414e] transition-all">
+            <div className="group bg-card p-2 rounded-xl cursor-pointer hover:bg-[#1e414e] transition-all">
               <Image className='w-full h-auto rounded-xl' src={ProductImage} alt='product-image' />
-              <h6 className='mt-4 mb-2 mx-2'>Movie 1</h6>
-              <p className='mb-2 mx-2'>2021</p>
+              <div className='flex justify-between items-start'>
+              <div className='mt-4 mb-2'>
+                <h6 className='mb-2 mx-2'>Movie 1</h6>
+                <p className='mx-2'>2021</p>
+              </div>
+              <button type="button" class="invisible group-hover:visible mt-4 text-white bg-primary hover:bg-primary-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Edit</button>
+              </div>              
             </div>
+
+           
         </div>
 
         <div className='flex justify-center items-center mt-32 flex-wrap'>
