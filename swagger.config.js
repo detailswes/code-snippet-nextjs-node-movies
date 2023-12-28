6c3e68db5f25cd254a425de1dc6e0b8f4c85ca57
details.wes <details.wes@gmail.com>
2023-12-28 11:42:46 +0530

    created api for getbyid movie

---
+++ swagger.config.js
@@ -0,0 +1,18 @@
+
+module.exports = {
+    openapi: '3.0.0',
+    info: {
+      title: 'Next.js Swagger Example',
+      version: '1.0.0',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+        description: 'Local Development Server',
+      },
+    ],
+    paths: {
+      // Define your API paths here
+    },
+  };
+  
